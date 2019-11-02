2c27080a4a8e69e5411153fb8f33656569cdc5f8
VeterinaryChen <513100163@qq.com>
2019-11-02 22:51:57 +0800

    项目初始化

---
+++ src/components/child/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe6ed;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe70a;</span>
+      输入城市/经典/游玩</div>
+    <div class="header-right">
+       城市<span class="iconfont down-icon">&#xe6f8;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header 
+    display: flex;
+    line-height: .86rem;
+    background:$bgColor;
+    color:#fff;
+    .header-left 
+      width: .64rem;
+      float :left;
+      .back-icon
+        text-align: center;
+        font-size: 0.5rem;
+    .header-input 
+      flex: 1;
+      height: .64rem ;
+      line-height:  .64rem;
+      background:#fff ;
+      margin-top:.12rem; 
+      margin-left:.2rem ;
+      background:#fff ;
+      border-radius:.1rem;
+      color: #ccc;
+    .header-right 
+      width :1.24rem;
+      float: right ;
+      text-align:center;
+      .down-icon
+        text-align: center;
+        font-size: 0.5rem;
+      
+</style>
