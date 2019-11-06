a44bc6073d79804f8488896950211824b4e1abb4
VeterinaryChen <513100163@qq.com>
2019-11-06 20:42:27 +0800

    增加城市选择页面搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input class="serach-input" type="text" placeholder="输入城市名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search{
+    height :.72rem;
+    background: $bgColor;
+    padding: 0 .1rem;
+    .serach-input{
+      box-sizing :border-box;
+      height: .62rem;
+      line-height :.62rem;
+      width : 100%;
+      text-align :center;
+      border-radius :.06rem;
+      color:#666;
+      padding :0 .1rem;
+    }
+  }
+</style>
